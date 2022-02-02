4e11fe9e552333a7e2f0c98c79096079cb7c72ff
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-02-02 16:52:22 +0700

    update

--- HomeWork/ex-02.js
+++ HomeWork/ex-02.js
@@ -1,10 +1,28 @@
-function equation(num) {
-    let number = ['one']
-    if (number = "one") {
-        num = 1;
+function equation(num){
+    const words = num.split(" ");
+    let number = [];
+    let oparater = [];
+    let sum = 0;
+    for (let index = 0; index < words.length; index++) {
+        if (words[index] == "one"){
+            number[index] = 1;
+        }else if(words[index] == "two"){
+            number[index] = 2;
+        }else if(words[index] == "+"){
+            oparater[index] = "+";
+        }
     }
-    return num;
+    
+    for (let index = 0; index < words.length; index++){
+        if(oparater[index] == "+"){
+            sum += number[index];
+        }
+    }
+    return sum;
 }
 
-let s = equation("one");
+
+let s = equation("one + one");
 console.log(s);
+let a = equation("one + two + three");
+console.log(a);
