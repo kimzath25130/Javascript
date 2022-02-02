a3baf4931fad3ed5e6900f35b14754e48f9065e6
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-02-02 14:12:24 +0700

    update

---
+++ HomeWork/ex-01.js
@@ -0,0 +1,10 @@
+function findPerimeter(length,width){
+    return (length+width)*2;
+}
+
+let sum = findPerimeter(6,7);
+console.log(sum);
+sum = findPerimeter(20,10);
+console.log(sum);
+sum = findPerimeter(2,9);
+console.log(sum);
