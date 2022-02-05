2f2b6d8437b7065965d489d8071b5e26910e3605
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-02-05 21:56:47 +0700

    update

--- HomeWork/ex-02.js
+++ HomeWork/ex-02.js
@@ -1,43 +1,39 @@
 function equation(num){
     const words = num.split(" ");
-    const number = [];
+    let number = "";
     let sum = 0;
     for (let index = 0; index < words.length; index++) {
         if (words[index] == "one"){
-            number[index] = 1;
+            number += "1";
         }else if(words[index] == "two"){
-            number[index] = 2;
+            number += "2";
         }else if(words[index] == "three"){
-            number[index] = 3;
+            number += "3";
         }else if(words[index] == "four"){
-            number[index] = 4;
+            number += "4";
         }else if(words[index] == "five"){
-            number[index] = 5;
+            number += "5";
         }else if(words[index] == "six"){
-            number[index] = 6;
+            number += "6";
         }else if(words[index] == "seven"){
-            number[index] = 7;
+            number += "7";
         }else if(words[index] == "eight"){
-            number[index] = 8;
+            number += "8";
         }else if(words[index] == "nine"){
-            number[index] = 9;
+            number += "9";
         }else if(words[index] == "+"){
-            number[index] = "+";
+            number += "+";
         }else if(words[index] == "-"){
-            number[index] = "-";
+            number += "-";
         }else if(words[index] == "*"){
-            number[index] = "*";
-        }
-
-        let n = 0;
-        for (let index = 1; index < number.length; index++){
-            if (number[index] == "+") {
-            }
-            index++
+            number += "*";
         }
     }
+    sum = eval(number);
     return sum;
 }
 
-let s = equation("one + six + three");
-console.log(s);
+console.log(equation("one + one"));
+console.log(equation("seven * four - two"));
+console.log(equation("one + one + one + one + one"));
+
--- HomeWork/test.js
+++ HomeWork/test.js
@@ -1,3 +1,8 @@
-var num = "7+5";
-console.log(parseInt(num)*1);
-console.log(Number(num));
+function equation(str){
+    let one=1,two=2,four=4,seven=7;
+    let result=eval(str);
+    return result;
+}
+console.log(equation("one-one+one"))
+console.log(equation("one+one+one"))
+console.log(eval("1+2+5"))
