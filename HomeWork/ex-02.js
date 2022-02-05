24cb28b1422f2ba2504d03a4bf56d9f54512543f
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-02-05 21:37:44 +0700

    update

--- HomeWork/ex-02.js
+++ HomeWork/ex-02.js
@@ -1,28 +1,43 @@
 function equation(num){
     const words = num.split(" ");
-    let number = [];
-    let oparater = [];
+    const number = [];
     let sum = 0;
     for (let index = 0; index < words.length; index++) {
         if (words[index] == "one"){
             number[index] = 1;
         }else if(words[index] == "two"){
             number[index] = 2;
+        }else if(words[index] == "three"){
+            number[index] = 3;
+        }else if(words[index] == "four"){
+            number[index] = 4;
+        }else if(words[index] == "five"){
+            number[index] = 5;
+        }else if(words[index] == "six"){
+            number[index] = 6;
+        }else if(words[index] == "seven"){
+            number[index] = 7;
+        }else if(words[index] == "eight"){
+            number[index] = 8;
+        }else if(words[index] == "nine"){
+            number[index] = 9;
         }else if(words[index] == "+"){
-            oparater[index] = "+";
+            number[index] = "+";
+        }else if(words[index] == "-"){
+            number[index] = "-";
+        }else if(words[index] == "*"){
+            number[index] = "*";
         }
-    }
-    
-    for (let index = 0; index < words.length; index++){
-        if(oparater[index] == "+"){
-            sum += number[index];
+
+        let n = 0;
+        for (let index = 1; index < number.length; index++){
+            if (number[index] == "+") {
+            }
+            index++
         }
     }
     return sum;
 }
 
-
-let s = equation("one + one");
+let s = equation("one + six + three");
 console.log(s);
-let a = equation("one + two + three");
-console.log(a);
---
+++ HomeWork/test.js
@@ -0,0 +1,3 @@
+var num = "7+5";
+console.log(parseInt(num)*1);
+console.log(Number(num));
