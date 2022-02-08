027b944f28121831783b0ac7e190ed0bc645f841
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-02-08 23:25:41 +0700

    update

--- HomeWork/test.js
+++ HomeWork/test.js
@@ -1,8 +1,14 @@
-function equation(str){
-    let one=1,two=2,four=4,seven=7;
-    let result=eval(str);
-    return result;
+/*let num = prompt("Enter you number : ");
+console.log("Hi "+num);
+let num2 = (Math.floor(Math.random()*100));
+document.getElementById("num2").innerHTML = num2;
+if (num == num2) {
+    alert("คุณถูกรางวัล");
+}else if(num != num2){
+    alert("คุณไม่ถูกรางวัล");
+}*/
+
+let age = prompt("Enter your age : ");
+if (age > 13) {
+    document.getElementById("scarry").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/YqgXVgRn_UI?start=249\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
 }
-console.log(equation("one-one+one"))
-console.log(equation("one+one+one"))
-console.log(eval("1+2+5"))
